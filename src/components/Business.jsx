81057d88d2050f2411b55fbcb10d29f2f27c15d8
WladiCosta <wladimircosta.dev@gmail.com>
2023-01-28 12:56:59 -0200

    improviment Business component page

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -3,6 +3,18 @@ import { features } from '../constants'
 import Button from './Button'
 
 
+const FeatureCard = ({ icon, title, content, index }) => (
+    <div className={`flex flex-row p-6 rounded-[20px] ${index !== features.length + 1 ? "mb-6" : "mb-0"} feature-card`}>
+      <div className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimBlue`}>
+        <img src={icon} alt="icon" className={`w-[50%] h-[50%] object-contain`}/>
+      </div>
+      <div className="flex-1 flex flex-col ml-3">
+        <h4 className="font-poppins font-semibold text-white text-[18px] leading-[23px] mb-1">{title}</h4>
+        <p className="font-poppins font-normal text-dimWhite text-[16px] leading-[24px] mb-1">{content}</p>
+      </div>
+    </div>
+  )
+
 const Business = ()  => (
     <section id={features} className={layout.section}>
       <div className={layout.sectionInfo}>
@@ -12,6 +24,11 @@ const Business = ()  => (
         building credit, earning rewards and saving money. But with hundreds
         of credit cards on the market.</p>
         <Button styles="mt-10"/>
+      </div>
+      <div className={`${layout.sectionImg} flex-col`}>
+        {features.map((feature, index) => (
+        <FeatureCard key={feature.id} {...feature} index={index}/>
+        ))}
       </div>
     </section>
     )
