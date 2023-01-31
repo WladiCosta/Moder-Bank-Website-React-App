1e5c3c235469e3e9b4b42a50cf36875b581fcdd5
Wladimir Costa <115286451+WladiCosta@users.noreply.github.com>
2023-01-31 18:19:07 -0200

    Update Testimonials.jsx

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -7,9 +7,9 @@ const Testimonials = () => (
     <section id="clients" className={`${styles.paddingY}${styles.flexCenter} flex-col relative`}>
       <div className="absolute z-[0] w-[60%] h-[60%] -right-[50%] rounded-full blue__gradient bottom-40"/>
       <div className="w-full flex justify-between items-center md:flex-row flex-col sm:mb-16 mb-6 relative z-[1]">
-        <h1 className={styles.heading2}>What peole are <br className="sm:block hidden"/>saying about us</h1>
+        <h1 className={styles.heading2}>What people are <br className="sm:block hidden"/>saying about us</h1>
         <div>
-          <p className={`${styles.paragraph} text-left max-w-[450px]`}>Everything you need to aceept card payments and grow your business anywhere on the planet</p>
+          <p className={`${styles.paragraph} text-left max-w-[450px]`}>Everything you need to accept card payments and grow your business anywhere on the planet</p>
         </div>
       </div>
       <div className="flex flex-wrap sm:justify-start justify-center w-full feedback-container relative z-[1]">
