d200372d7314b534bbdf1a5671e867ff4ac7bb06
Wladimir Costa <115286451+WladiCosta@users.noreply.github.com>
2023-01-31 18:18:20 -0200

    Update CardDeal.jsx

--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -6,7 +6,7 @@ import Button from './Button'
 const CardDeal = () => (
     <section className={layout.section}>
       <div className={layout.sectionInfo}>
-        <h2 className={styles.heading2}>Find a bitter card deal <br className="sm:block hidden"/> in a few easy steps.</h2>
+        <h2 className={styles.heading2}>Find a better card deal <br className="sm:block hidden"/> in a few easy steps.</h2>
         <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Find a better card deal easily: research options, check credit score, compare offers, read terms, choose the best fit and apply.</p>
         <Button styles="mt-10" />
       </div>
